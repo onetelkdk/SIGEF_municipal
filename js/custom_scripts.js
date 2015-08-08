6524b53225d7c3ad0f238aa7b8ef33879c191a8a
Hector Lara (Alex) <djnegrosabio@gmail.com>
2015-08-08 10:34:09 -0400

    .

--- js/custom_scripts.js
+++ js/custom_scripts.js
@@ -1,8 +1,6 @@
 $(document).ready (function(){
   //Cargar menu
   $( "#sidebar-menu" ).load( "menu_principal.html" );
-  
-  
 });
 
 
@@ -20,12 +18,12 @@ $(function() {
   $('#control_menu').click(function() {
     if (s == 0) {
       s = 1;
-      $( "#sidebar-menu" ).css({"display": "none"},100);
-      $('#page-wrapper').animate({'margin-left': "0px"}, 100);
+      $( "#sidebar-menu" ).css({"display": "none"},200);
+      $('#page-wrapper').animate({'margin-left': "0px"}, 200);
     } else {
       s = 0;
-      $( "#sidebar-menu" ).css({"display": "block"},100);
-      $('#page-wrapper').animate({'margin-left': "300px"}, 100);
+      $( "#sidebar-menu" ).css({"display": "block"},200);
+      $('#page-wrapper').animate({'margin-left': "300px"}, 200);
     }
   });
 });
