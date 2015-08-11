2cf950ab277f319b9c7bb8221b1576c738dd1d8a
Edwin <mardwintrader@gmail.com>
2015-08-11 13:09:14 -0400

    Update EDG

--- js/custom_scripts.js
+++ js/custom_scripts.js
@@ -36,13 +36,25 @@ $('#editar').click(function() {
     $( ".contenido-inicial" ).css({"display": "none"},500);
     $( "#div-editar" ).css({"display": "block"},500);
     $( "#sidebar-menu" ).css({"display": "none"},100);
-     $('#page-wrapper').animate({'margin-left': "0px"}, 100);
+    $('#page-wrapper').animate({'margin-left': "0px"}, 100);
+    $( "#crear" ).css({"display": "none"},500);
+    $( "#editar" ).css({"display": "none"},500);
+    $( "#visualizar" ).css({"display": "none"},500);
+    $( "#guardar" ).css({"display": "block"},500);
+    $( "#guardar-nuevo" ).css({"display": "block"},500);
+    $( "#cancelar" ).css({"display": "block"},500);
   });
 $('#cancelar').click(function() {
     $( ".contenido-inicial" ).css({"display": "block"},500);
     $( "#div-editar" ).css({"display": "none"},500);
     $( "#sidebar-menu" ).css({"display": "block"},100);
     $('#page-wrapper').animate({'margin-left': "300px"}, 100);
+    $( "#crear" ).css({"display": "block"},500);
+    $( "#editar" ).css({"display": "block"},500);
+    $( "#visualizar" ).css({"display": "block"},500);
+    $( "#guardar" ).css({"display": "none"},500);
+    $( "#guardar-nuevo" ).css({"display": "none"},500);
+    $( "#cancelar" ).css({"display": "none"},500);
   });
 $('#visualizar').click(function() {
     $( ".contenido-inicial" ).css({"display": "none"},500);
@@ -54,6 +66,12 @@ $('#cerrar').click(function() {
     $( ".contenido-inicial" ).css({"display": "block"},500);
     $( "#div-visualizar" ).css({"display": "none"},500);
     $( "#sidebar-menu" ).css({"display": "block"},100);
+    $( "#crear" ).css({"display": "block"},500);
+    $( "#editar" ).css({"display": "block"},500);
+    $( "#visualizar" ).css({"display": "block"},500);
+    $( "#guardar" ).css({"display": "none"},500);
+    $( "#guardar-nuevo" ).css({"display": "none"},500);
+    $( "#cancelar" ).css({"display": "none"},500);
   });
 });
 
